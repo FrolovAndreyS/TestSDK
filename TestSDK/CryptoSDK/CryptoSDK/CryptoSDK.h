6da21689ef9fe9c1cf0a463021adfed0f306cb55
Andrey Frolov <frolov@surfstudio.ru>
2022-03-15 10:35:27 +0300

    Initial commit

---
+++ TestSDK/CryptoSDK/CryptoSDK/CryptoSDK.h
@@ -0,0 +1,18 @@
+//
+//  CryptoSDK.h
+//  CryptoSDK
+//
+//  Created by frolov on 15.03.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CryptoSDK.
+FOUNDATION_EXPORT double CryptoSDKVersionNumber;
+
+//! Project version string for CryptoSDK.
+FOUNDATION_EXPORT const unsigned char CryptoSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CryptoSDK/PublicHeader.h>
+
+
